34a415ae22aa2be11c4c7ebffb4705296b8f6e96
ddjamalova <53920815+ddjamalova@users.noreply.github.com>
2019-09-04 17:43:05 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -6,15 +6,31 @@ from optparse import OptionParser
 
 faa = '.faa'
 fasta = '.fasta'
-def change_format(path, faa, fasta):
+def getfasta(path, faa, fasta):
     for files in os.listdir(path):
         if files.endswith(faa):
             src = files.split(faa)[0]
             source = files.strip()
-            os.chdir(outpath)
             os.rename(path + source, path + src + fasta)
+    
+def getfaa(path, faa, fasta):
+    for files in os.listdir(path):
+        if files.endswith(fasta):
+            src = files.split(faa)[0]
+            source = files.strip()
+            os.rename(path + source, path + src + faa)
 parser = OptionParser()
-parser.add_option("-p", "--path", help="Directory with .faa files")
+parser.add_option("-t", "--task", help="""task code:\n\t\t\t\t\t
+=fasta to get .fasta files.
+\t\t\t\t\t=faa to get .faa files.\n
+\n\t\t\t\t\tRequires options:
+\n\t\t\t\t\t-p : Directory with files""")
+parser.add_option("-p", "--path", help="Directory with fasta files")
 opt, args = parser.parse_args()
-change_format(opt.path, faa, fasta)
+print(opt.task)
+if opt.task == "fasta":
+    getfasta(opt.path, faa, fasta)
+
+if opt.task == "faa":
+    getfaa(opt.path, faa, fasta)
 print("Done")
